3535881f6fa9a4f3576178e5f851e41a6aaa5ddb
afelicia <afelicia@student.42madrid.com>
2023-05-17 01:39:49 +0200

    functions addded

--- includes/fractol.h
+++ includes/fractol.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afelicia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 22:45:45 by afelicia          #+#    #+#             */
-/*   Updated: 2023/05/03 19:07:53 by afelicia         ###   ########.fr       */
+/*   Updated: 2023/05/12 14:34:53 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,21 @@ typedef struct	s_data {
 	double	sx;
 	double	rx;
 	double	fx;
+	int		code;
 }	t_data;
 
 int	color_converter(const char *str);
+int	close_esc(t_data *data);
+void ret_error(char *error_msg);
+int	mousecode(int button, int x, int y, t_data *data);
+int	keycodes(int keycode, t_data *data);
+int	mandelbrot(t_data *data, double cr, double ci);
+int	julia(t_data *data);
+//int	tricorn(double cr, double ci);
+int	mandelbox(t_data *f, double cr, double ci);
+void	painting(int size, t_data *data, int color);
+void	render(t_data *data);
+
+
 
 #endif
